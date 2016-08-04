d65310d577cb1ed811e95d1605ab49be0fbf7e1c
rodralez <rodralez@gmail.com>
2016-08-04 13:52:26 -0300

    2016-08-04

--- acc_nav2body.m
+++ acc_nav2body.m
@@ -2,42 +2,42 @@ function acc_b = acc_nav2body (acc_n, DCMnb)
 % acc_nav2body: transform accelerations from navigation frame to body
 % 				frame.
 %
-%   Copyright (C) 2014, Rodrigo Gonzalez, all rights reserved. 
-%     
-%   This file is part of NaveGo, an open-source MATLAB toolbox for 
+%   Copyright (C) 2014, Rodrigo Gonzalez, all rights reserved.
+%
+%   This file is part of NaveGo, an open-source MATLAB toolbox for
 %   simulation of integrated navigation systems.
-%     
+%
 %   NaveGo is free software: you can redistribute it and/or modify
-%   it under the terms of the GNU Lesser General Public License (LGPL) 
+%   it under the terms of the GNU Lesser General Public License (LGPL)
 %   version 3 as published by the Free Software Foundation.
-% 
+%
 %   This program is distributed in the hope that it will be useful,
 %   but WITHOUT ANY WARRANTY; without even the implied warranty of
 %   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %   GNU Lesser General Public License for more details.
-% 
-%   You should have received a copy of the GNU Lesser General Public 
-%   License along with this program. If not, see 
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this program. If not, see
 %   <http://www.gnu.org/licenses/>.
 %
-% Reference: 
-%			R. Gonzalez, J. Giribet, and H. Patiño. NaveGo: a 
-% simulation framework for low-cost integrated navigation systems, 
-% Journal of Control Engineering and Applied Informatics}, vol. 17, 
+% Reference:
+%			R. Gonzalez, J. Giribet, and H. Patiño. NaveGo: a
+% simulation framework for low-cost integrated navigation systems,
+% Journal of Control Engineering and Applied Informatics}, vol. 17,
 % issue 2, pp. 110-120, 2015. Eq. 10.
 %
 % Version: 001
 % Date:    2014/09/11
 % Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
-% URL:     https://github.com/rodralez/navego 
+% URL:     https://github.com/rodralez/navego
 
 acc_b = zeros(size(acc_n));
 
 for k = 1:max(size(acc_n)),
     
-    DCMnb1 = reshape(DCMnb(k,:), 3, 3); 
+    DCMnb1 = reshape(DCMnb(k,:), 3, 3);
     acc_b(k,:) = ( DCMnb1 * ( acc_n(k,:)') )';
-
+    
 end
 
 end
