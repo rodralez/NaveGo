12deab07716d13a67732c8e0517bc255f72433be
rodralez <rodralez@gmail.com>
2016-08-13 10:03:01 -0300

    2016-08-13

--- gyro_gen.m
+++ gyro_gen.m
@@ -1,8 +1,8 @@
 function wb = gyro_gen (ref, imu)
-% gyro_gen: simulates gyro measurements from reference data and 
+% gyro_gen: generates simulated gyros measurements from reference data and 
 %          imu error profile.
 %
-%   Copyright (C) 2014, Rodrigo Gonzalez, all rights reserved. 
+%   Copyright (C) 2014, Rodrigo González, all rights reserved. 
 %     
 %   This file is part of NaveGo, an open-source MATLAB toolbox for 
 %   simulation of integrated navigation systems.
@@ -26,11 +26,8 @@ function wb = gyro_gen (ref, imu)
 % Journal of Control Engineering and Applied Informatics, vol. 17, 
 % issue 2, pp. 110-120, 2015. Sec. 2.1.
 %
-%           K. Jerath, Noise Modeling of Sensors: The Allan Variance
-% Method. Presentation. Page 40. URL: eecs.wsu.edu/~taylorm/16_483/Jerath.pptx
-%
-% Version: 003
-% Date:    2016/05/31
+% Version: 002
+% Date:    2015/08/20
 % Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
 % URL:     https://github.com/rodralez/navego 
 
@@ -38,21 +35,17 @@ function wb = gyro_gen (ref, imu)
 if (isfield(ref, 'wb'))    
     
     gyro_b = ref.wb;
-    
-    N = ref.kn;
+
 % Obtain gyros from DCM    
 else
     gyro_raw = gyro_gen_delta(ref.DCMnb, diff(ref.t));
-%     gyro_raw = [0 0 0; gyro_raw;];
-    gyro_b  = sgolayfilt(gyro_raw, 15, 49);
-    
-    N = ref.kn - 1;
+    gyro_raw = [gyro_raw; 0 0 0;];
+    gyro_b  = sgolayfilt(gyro_raw, 15, 299);
 end
 
 % omega_en_n and omega_ie_n
-gyro_err_b = zeros(N, 3); 
-
-for i = 1:N,
+gyro_err_b = zeros(ref.kn, 3); 
+for i = 1:ref.kn,
     
     dcmnb = reshape(ref.DCMnb(i,:), 3, 3); 
     omega_ie_n = earthrate(ref.lat(i));
@@ -61,40 +54,39 @@ for i = 1:N,
     gyro_err_b(i,:) = ( omega_in_b )';   
 end
 
-% Set static bias randomly from the interval [-fix fix]
+% Set static bias randomly from interval
 a = -imu.gb_fix(1);
 b = imu.gb_fix(1);
 gb_fix = (b-a).*rand(3,1) + a;
 
 % Random vectors
-r1=randn(N,1);
-r2=randn(N,1);
-r3=randn(N,1);
-r4=randn(N,1);
-r5=randn(N,1);
-r6=randn(N,1);
-
-o=ones(N,1);
+r1=randn(ref.kn,1);
+r2=randn(ref.kn,1);
+r3=randn(ref.kn,1);
+r4=randn(ref.kn,1);
+r5=randn(ref.kn,1);
+r6=randn(ref.kn,1);
+o=ones(ref.kn,1);
 
 if (isinf(imu.gcorr))
     sigmc = imu.gb_drift;
-    b_drift = [sigmc(1).*r4 sigmc(2).*r5 sigmc(3).*r6];
+    gcorr = [sigmc(1).*r4 sigmc(2).*r5 sigmc(3).*r6];
 else
     % Gyro correlation noise
-    b_drift = zeros(N,3);
+    gcorr = zeros(ref.kn,3);
     dt=mean(diff(imu.t));
     alpha = exp(-dt./imu.gcorr);
     sigmc = imu.gb_drift .* sqrt(1 - alpha.^2);
-    b_drift(1,:) = gb_fix' + [sigmc(1).*r4(1) sigmc(2).*r5(1) sigmc(3).*r6(1)];
+    gcorr(1,:) = gb_fix' + [sigmc(1).*r4(1) sigmc(2).*r5(1) sigmc(3).*r6(1)];
 
-    for i=2:N    
-        b_drift (i,:) = alpha .* b_drift (i-1,:) + [sigmc(1).*r4(i) sigmc(2).*r5(i) sigmc(3).*r6(i)]; 
+    for i=2:ref.kn    
+        gcorr (i,:) = alpha .* gcorr (i-1,:) + [sigmc(1).*r4(i) sigmc(2).*r5(i) sigmc(3).*r6(i)]; 
     end
 end
 
 wb = gyro_b + gyro_err_b + ...
      [imu.gstd(1).*r1    imu.gstd(2).*r2    imu.gstd(3).*r3 ] + ...
      [gb_fix(1).*o   gb_fix(2).*o   gb_fix(3).*o] + ...
-     b_drift; 
+     gcorr; 
 
 end
