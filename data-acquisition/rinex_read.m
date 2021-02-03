8b2a923ac5fa039fc5c9c2366ac7c5ef2b7ac077
Rodrigo <rodralez@gmail.com>
2021-02-03 16:57:30 -0300

    2021-02-03

---
+++ data-acquisition/rinex_read.m
@@ -0,0 +1,310 @@
+function gnss = rinex_read(fname)
+% rinex_read: reads data from RINEX observation file (v2.11) and organize
+% them into a particular data structure with raw GPS data, i.e., pseudo 
+% ranges, carrier phase and pseudo-range rates.
+%
+% This function assumes that the Teqc program is installed on the 
+% current file system and its instalation directory is available on the
+% system path.
+%
+% L1 and C1 data are taken from GPS system only.
+%
+% INPUT
+%   fname: RINEX file name (string).
+%
+% OUTPUT
+%   gnss: data structure with the following format. 
+%
+%      raw: Nx15 rinex data ordered into columns as:
+%           1: year.
+%           2: month.
+%           3: day.
+%           4: hour.
+%           5: minutes.
+%           6: seconds.
+%           7: number of satellite in view.
+%           8: C1 pseudo-range using C/A Code on L1 (meters).
+%           9: C1 pseudo-range signal strength (0-9) where, 
+%               0 or blank: not known, don't care. 
+%               1: minimum possible signal strength.          
+%               5: threshold for good S/N ratio.              
+%               9: maximum possible signal strength.          
+%           10: L1 phase measurements on L1 (full cycles).
+%           11: L1 phase signal strength (0-9).
+%           12: pseudo-range rate on L1 (meters/s).
+%       
+%      header, RINEX file header.   
+%
+% Each epoch at the raw matrix may have information for several satellites.
+%
+%   Copyright (C) 2014, Rodrigo Gonzalez, all rights reserved.
+%
+%   This file is part of NaveGo, an open-source MATLAB toolbox for
+%   simulation of integrated navigation systems.
+%
+%   NaveGo is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU Lesser General Public License (LGPL)
+%   version 3 as published by the Free Software Foundation.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this program. If not, see
+%   <http://www.gnu.org/licenses/>.
+%
+% References:
+%  
+%   UNACOV. Teqc Tutorial, Basics of Teqc Use and Teqc Products. June 6, 
+% 2014. 
+%
+%   Mark Petovello. How does a GNSS receiver estimate velocity?
+% InsideGNSS Magazine. March/April 2015.
+%
+%   Werner Gurtner. RINEX: The Receiver Independent Exchange Format
+% Version 2.11 (v2). June 26, 2012.
+%   
+%   Paul Groves, Principles of GNSS, Inertial, and
+% Multisensor Integrated Navigation Systems (2008). Chapter 7.
+%
+% Version: 001
+% Date:    2018/12/07
+% Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
+% URL:     https://github.com/rodralez/navego
+
+%% CONSTANTS
+
+L1 = 1575.42;       % L1 carrier frequency (Hz)
+L2 = 1227.60;       % L2 carrier frequency (Hz)
+C  = 299792458;     % Speed of light (m/s)
+
+%% RINEX FILE
+
+fid = fopen(fname, 'r');
+if fid == -1
+    error('rinex_read: %s file not found', fname)
+end
+
+% TOTAL NUMBER OF LINES
+
+lines = nnz(fread(fid) == 10);
+fprintf('rinex_read: %s file has %d lines. \n', fname, lines);
+
+% Set pointer back to the beginning
+fseek(fid,0,'bof');
+
+% HEADER
+
+hdlines = 0;
+idx = [];
+header = {};
+
+while ( isempty(idx) )
+    
+    line = fgetl(fid);
+    idx = find(contains(line,'END OF HEADER'));
+    hdlines = hdlines + 1;
+    header(hdlines, :) = cellstr (line);
+end
+
+fprintf('rinex_read: %s has %d header lines. \n', fname, hdlines);
+
+% Set pointer back to the beginning
+fseek(fid,0,'bof');
+
+% SEARCH FOR INTERVAL TIME IN HEADER
+
+flag = true;
+
+while (flag)
+    
+    line = fgetl(fid);
+    flag = ~ (contains(line,'INTERVAL'));
+end
+
+line_h_time = strsplit(line);
+dt = str2double (line_h_time(2));   % INTERVAL
+
+fprintf('rinex_read: interval time is %f. \n', dt);
+
+% Set pointer back to the beginning
+fseek(fid,0,'bof');
+
+fclose(fid);
+
+%% TEQC COMMAND
+
+% -G elimate all GPS SVs and data *
+% -R elimate all GLONASS SVs and data *
+% -E elimate all Galileo/GPS SVs and data *
+% -S elimate all SBAS SVs and data *
+% -C elimate all Beidou­2/Compass SVs and data *
+% -J elimate all QZSS SVs and data *
+
+% L1, L2: Phase measurements on L1 and L2
+% C1, C2: Pseudorange using C/A Code on L1,L2
+% P1, P2: Pseudorange using P-Code on L1,L2
+% D1, D2: Doppler frequency on L1 and L2
+% T1, T2: Transit Integrated Doppler on 150 (T1) and 400 MHz (T2)
+% S1, S2: Raw signal strengths or SNR values as given by the receiver for the L1,L2 phase observations
+
+file_teqc = sprintf('temp_%s', fname);
+
+fprintf('rinex_read: creating %s file with C1 and L1 data. \n', file_teqc);
+
+command = sprintf('teqc -O.obs C1L1 -R -E -S ./%s > %s', fname, file_teqc);
+
+[status,~] = system(command);
+if status ~= 0
+    error('rinex_read: teqc command fails')
+end
+
+%% TEQC FILE HEADER
+
+fid = fopen(file_teqc, 'r');
+if fid == -1
+    error('rinex_read: %s file not found', file_teqc)
+end
+
+% SEARCH FOR TIME OF FIRST OBS
+flag = true;
+
+while (flag)
+    
+    line = fgetl(fid);
+    flag = ~ (contains(line,'TIME OF FIRST OBS'));
+end
+
+line_h_time = strsplit(line);
+year = cell2mat (line_h_time(2));
+
+% SEARCH FOR FIRST ROW OF DATA
+
+flag = true;
+
+while (flag)
+    
+    line = fgetl(fid);
+    flag = ~ (contains(line, year(3:4)));
+end
+
+% Set pointer back one row
+fseek(fid,-1,'cof');
+
+%% TEQC FILE BODY
+
+data = zeros (lines, 12);
+row = 1;
+
+fprintf('rinex_read: getting RINEX data from body file...\n');
+
+while ~feof(fid)
+    
+    line = fgetl(fid);
+    
+    % Take epoch time
+    line_p_time = strsplit(line);
+    line_size = max(size(line_p_time));
+    
+    % Check time line integrity
+    if line_size > 8
+        
+        % Fill data with epoch time
+        for idx = 2:7
+            data (row, idx-1) = str2double ( cell2mat (line_p_time(idx)) );
+        end
+        
+        % Number of satellites in epoch
+        sat_num = strsplit(line_p_time{9},'G');
+        sat_total = str2double(sat_num{1});
+        
+        % For each satellite in view, fill one row with satellite data
+        for idx = 1:sat_total
+            
+            data (row, 7) = str2double(sat_num{idx+1});     % Satellite number
+            line = fgetl(fid);
+            
+            if ~isempty(line)
+                
+                line_p_data = textscan(line,'%f %f %f %f');  % C1 L1
+                
+                for jdx = 1:4
+                    n = line_p_data{jdx};
+                    if ~isempty(n)
+                        data (row, jdx+7) = n;
+                    end
+                end
+            end
+            
+            row = row + 1;
+        end
+    end    
+end
+
+fclose(fid);
+
+%% PSEUDO-RANGE RATES
+
+fprintf('rinex_read: calculating pseudo-range rates... \n');
+
+sat_v = nonzeros(unique(data(:,7)));
+
+M = length(sat_v);
+
+for k = 1:M
+   
+    sat_num = sat_v(k);
+    
+    idx = (data(:,7) == sat_num);
+    
+    phase_l1 = data(idx,10);
+    
+    % [Petovello, Eq. 8]
+    doppler_l1 = diff (phase_l1) ./ dt;
+    
+    % [Groves, Eq. 7.129]
+    prr_l1 = - doppler_l1 .* C ./ L1 ; 
+    
+    % Since prr_l1 has one less element than phase_l1, first row with sat
+    % data should not be addressed
+    
+    % Find the first row with sat data
+    jdx = find (idx == true, 1, 'first');
+    
+    idx(jdx) = false;
+    
+    data(idx,12) = prr_l1;
+    
+end
+
+%% 
+
+% Detect non-empty rows
+idx = data(:, 7) ~= 0;
+
+% Delete empty rows
+data = data(idx, :);
+
+gnss.raw = data;
+
+gnss.header = header;
+
+%% QUALITY
+
+c1_ss = data(:,9);
+c1_ss_median = median(c1_ss);
+c1_ss_mean = mean(c1_ss);
+
+fprintf('rinex_read: median of C1 pseudo-ranges signal strength is %f...\n', c1_ss_median);
+fprintf('rinex_read: median of C1 pseudo-ranges signal strength is %f...\n', c1_ss_mean);
+
+l1_ss = data(:,11);
+l1_ss_median = median(l1_ss);
+l1_ss_mean = mean(l1_ss);
+
+fprintf('rinex_read: median of L1 phase measurements signal strength is %f...\n', l1_ss_median);
+fprintf('rinex_read: median of L1 phase measurements signal strength is %f...\n', l1_ss_mean);
+
+
---
+++ data-acquisition/xsens_read.m
@@ -0,0 +1,295 @@
+function [xsens , xsens_fields ] = xsens_read(file, row_lines, columns)
+% xsens_read: reads .cvs files created by Xsens software.
+%
+% INPUT
+%   fname: .cvs file name (string).
+%   blockMatrix:
+%   columns: maximum column to be read.
+%
+% OUTPUT
+%   xsens: data structure where each field is a column found in the file.
+%   xsens_fields: cell vector with the names of each field found in the file.
+%
+%   Copyright (C) 2014, Rodrigo Gonzalez, all rights reserved.
+%
+%   This file is part of NaveGo, an open-source MATLAB toolbox for
+%   simulation of integrated navigation systems.
+%
+%   NaveGo is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU Lesser General Public License (LGPL)
+%   version 3 as published by the Free Software Foundation.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU Lesser General Public License for more details.
+%
+%   You should have received a copy of the GNU Lesser General Public
+%   License along with this program. If not, see
+%   <http://www.gnu.org/licenses/>.
+%
+% Reference:
+%
+%
+% Version: 002
+% Date:    2020/10/30
+% Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
+% URL:     https://github.com/rodralez/navego
+
+disp('xsens_read: processing data...')
+
+%% OPEN FILE
+
+xsens_f = fopen(file, 'r');
+if xsens_f == -1
+    error('xsens_read: ERROR: %s not found', file)
+else
+    fprintf('xsens_read: Loading %s...\n', file)
+end
+
+fprintf('xsens_read: %s has %d lines.\n', file, row_lines)
+
+%% HEADER
+
+header_rows = 13;
+
+xsens_header = cell(header_rows,1);
+for a = 1:header_rows
+    xsens_header{a,1} = fgets(xsens_f);
+end
+
+%% FIELDS
+
+xsens_line = fgets(xsens_f);
+xsens_line = strrep(xsens_line,'[','_');    % Chance '[' for '_'
+xsens_line = strrep(xsens_line,']','');     % Delete ']'
+
+xsens_fields = textscan(xsens_line, '%s', 'Delimiter', ',' , 'EmptyValue', 0);
+
+xsens_fields = xsens_fields{1};
+xsens_fields(83) = {'GNSSLongitude'};
+xsens_fields(84) = {'GNSSLatitude'};
+
+[N,~] = size (xsens_fields);
+
+fprintf('xsens_read: %s has %d columns.\n', file, N);
+
+fprintf('xsens_read: only %d columns will be processed.\n', columns);
+
+%% GET DATA
+
+% Set pointer back to the beginning of file
+% fseek(xsens_f,0,'bof');
+
+% Pattern for textscan()
+formatSpec = repmat('%f ', 1, N);
+
+% Correction of numbers of rows
+row_lines = row_lines - header_rows;
+
+% Preallocation
+xsens_imu = zeros(row_lines, columns);
+xsens_gnss = zeros(floor(row_lines/2), columns);
+xsens_ahrs = zeros(floor(row_lines/2), columns);
+xsens_mag = zeros(floor(row_lines/2), columns);
+xsens_att = zeros(floor(row_lines/2), columns);
+xsens_baro = zeros(floor(row_lines/2), columns);
+
+% Index for data allocation inside the while loop.
+a = 1;  % index for reading IMU data
+% b
+c = 1;  % index for reading GNSS data
+% d
+e = 1;  % index for reading MAG data
+% f
+g = 1;  % index for reading AHRS data
+% h
+i = 1;  % index for reading ATTITUDE data
+% j
+k = 1;  % index for reading BARO data
+% l
+
+rows_block = 75000;
+
+if rows_block > row_lines
+    rows_block = floor(row_lines / 5);
+end
+
+fprintf('xsens_read: processing %s...\n', file)
+
+loop_line = 1;
+loop_ctr = 0;
+
+while ~feof(xsens_f)
+    
+    fprintf('xsens_read: processing line %d...\n',  loop_line)
+    loop_ctr = loop_ctr + 1;
+    loop_line = loop_ctr * rows_block;
+    
+    data_c = textscan(xsens_f, formatSpec, rows_block, 'Delimiter',','); % ,'EmptyValue',0
+    data = cell2mat(data_c(1:columns));
+    
+    % Delete rows where date is NaN for IMU data
+    idl = ~isnan(data(: , 18));     % Column 18 is Acc_X
+    data_imu = data(idl , :);
+    
+    % Delete rows where the difference between two adjacent times is negative
+    idl = diff(data_imu(: , 2)) > 0;
+    idl = [true; idl];
+    data_imu = data_imu(idl , :);
+    
+    % Delete rows where date is NaN for GNSS data
+    idl = ~isnan(data(: , 70));     % Column 70 is NUTimeOfWeek
+    data_gnss = data(idl , :);
+    
+    % Delete rows where date is NaN for MAG data
+    idl = ~isnan(data(: , 33));     % Column 33 is Mag_X
+    data_mag = data(idl , :);
+    
+    % Delete rows where date is NaN for AHRS data
+    idl = ~isnan(data(: , 24));     % Column 24 is AccHR_X
+    data_ahrs = data(idl , :);
+    
+    % Delete rows where date is NaN for ATTITUDE data
+    idl = ~isnan(data(: , 44));     % Column 44 is Quat_q0
+    data_att = data(idl , :);
+    
+    % Delete rows where date is NaN for BARO data
+    idl = ~isnan(data(: , 43));     % Column 43 is Pressure
+    data_baro = data(idl , :);
+    
+    [M, ~] = size( data_imu );
+    b = a + M - 1;
+    xsens_imu(a:b , :) = data_imu;
+    a = b + 1;
+    
+    [M, ~] = size( data_gnss );
+    d = c + M - 1;
+    xsens_gnss(c:d , :) = data_gnss;
+    c = d + 1;
+    
+    [M, ~] = size( data_mag );
+    f = e + M - 1;
+    xsens_mag(e:f , :) = data_mag;
+    e = f + 1;
+    
+    [M, ~] = size( data_ahrs );
+    h = g + M - 1;
+    xsens_ahrs(g:h , :) = data_ahrs;
+    g = h + 1;
+    
+    [M, ~] = size( data_att);
+    j = i + M - 1;
+    xsens_att(i:j , :) = data_att;
+    i = j + 1;
+    
+    [M, ~] = size( data_baro);
+    l = k + M - 1;
+    xsens_baro(k:l , :) = data_baro;
+    k = l + 1;
+    
+    clear data_c data data_vld idl data_imu data_gnss data_mag data_ahrs
+    
+end
+
+% Delete epmty rows
+idl = xsens_imu(: , 1) ~= 0.0 ;
+xsens_imu = xsens_imu(idl , :);
+
+idl = xsens_gnss(: , 1) ~= 0.0 ;
+xsens_gnss = xsens_gnss(idl , :);
+
+idl = xsens_mag(: , 1) ~= 0.0 ;
+xsens_mag = xsens_mag(idl , :);
+
+idl = xsens_ahrs(: , 1) ~= 0.0 ;
+xsens_ahrs = xsens_ahrs(idl , :);
+
+idl = xsens_att(: , 1) ~= 0.0 ;
+xsens_att = xsens_att(idl , :);
+
+idl = xsens_baro(: , 1) ~= 0.0 ;
+xsens_baro = xsens_baro(idl , :);
+
+%% DATA IN STRUCTURE
+
+xsens = struct;
+
+% IMU DATA
+for a=1:23
+    xsens.(xsens_fields{a}) = xsens_imu(:, a);
+end
+
+for a=27:29
+    xsens.(xsens_fields{a}) = xsens_imu(:, a);
+end
+
+for a=36:42
+    xsens.(xsens_fields{a}) = xsens_imu(:, a);
+end
+
+for a=60:66
+    xsens.(xsens_fields{a}) = xsens_imu(:, a);
+end
+
+% AHRS DATA
+for a=24:26
+    xsens.(xsens_fields{a}) = xsens_ahrs(:, a);
+end
+
+for a=30:32
+    xsens.(xsens_fields{a}) = xsens_ahrs(:, a);
+end
+
+% MAG DATA
+for a=33:35
+    xsens.(xsens_fields{a}) = xsens_mag(:, a);
+end
+
+% ATTITUDE DATA
+for a=44:59
+    xsens.(xsens_fields{a}) = xsens_att(:, a);
+end
+
+% BARO DATA
+xsens.(xsens_fields{43}) = xsens_baro(:, 43);
+
+% GNSS DATA
+for a=70:columns
+    xsens.(xsens_fields{a}) = xsens_gnss(:, a);
+end
+
+xsens.SampleTimeFine        = xsens_imu(:, 2);      % SampleTimeFine
+xsens.AHRSSampleTimeFine    = xsens_ahrs(:, 2);     % SampleTimeFine
+xsens.MagSampleTimeFine     = xsens_mag(:, 2);      % SampleTimeFine
+xsens.AttitudeSampleTimeFine = xsens_att(:, 2);     % SampleTimeFine
+xsens.BaroSampleTimeFine    = xsens_baro(:, 2);     % SampleTimeFine
+xsens.GNSSSampleTimeFine    = xsens_gnss(:, 2);
+
+% FREQUENCIES
+dti = median(diff(xsens.SampleTimeFine)) * 10e-5;
+xsens.freq_imu = (1/dti);
+
+dta = median(diff(xsens.AHRSSampleTimeFine)) * 10e-5;
+xsens.freq_ahrs = (1/dta);
+
+dtm = median(diff(xsens.MagSampleTimeFine)) * 10e-5;
+xsens.freq_mag = (1/dtm);
+
+dtt = median(diff(xsens.AttitudeSampleTimeFine)) * 10e-5;
+xsens.freq_att = (1/dtt);
+
+dtb = median(diff(xsens.BaroSampleTimeFine)) * 10e-5;
+xsens.freq_baro = (1/dtb);
+
+dtg = median(diff(xsens.NUTimeOfWeek)) * 10e-4;
+xsens.freq_gnss = (1/dtg);
+
+% Header
+xsens.header = xsens_header;
+
+fclose(xsens_f);
+
+disp('xsens_read: data is ready.')
+
+end
